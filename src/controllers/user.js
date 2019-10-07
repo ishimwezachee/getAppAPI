e86c0fb64cc548cac4b6ea4ffaf61098d4a4e6e4
zachee <zacheeishimwe@gmail.com>
2019-10-07 13:49:31 +0200

    signIn and signUp

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -7,78 +7,57 @@ import Validation from "../validations/validation";
 
 class UserController{
     static signup(req,res){
+        // fetch the data from the the body reqyest
         const {firstName,surName,telephone,email,nationality,category}=req.body;
- 
+        // check on the validation;
      const validationObject = {firstName,surName,telephone,email,nationality,category};
      const { error } = Validation.SignUpValidation(validationObject);
     if(error){
         res.status(400).json({status:400,error:`issue made ${error}`});
     }else{
-     // check if the email exits    :
+     // check the user by phoneNumber
      const user = UserModel.findUser(telephone);
      if(user){
-         res.status(400).json({status:400,error:"email already exists"})
-     }else{// store data with hashed telphoneNumber;
-        const saltRounds = 10;
-        bcrypt.hash(telephone,saltRounds,(err,hashedTelephone)=>{
-            if(err){
-                res.status(500).json({status:500,error:"server error"});
-            }else{
-        const userObject ={firstName,surName,hashedTelephone,email,nationality,category};
-        const newUser = UserModel.createUser(userObject);
-        // generate jwt
-        const token = jwt.sign({userId:newUser.id,telephone},"RANDOM_TOKEN_SECRET",{ expiresIn:"24"});
-         // final response
-         res.status(201).json({
-             status:201,
-             data:{token,id:newUser.id,firstName,surName,hashedTelephone,email,nationality,category}
-         });
+         res.status(400).json({status:400,error:"phoneNumber already exists"})
+     }else{
+         // creating a new user;
+         const userObject = {firstName,surName,telephone,email,nationality,category};
+         const newUser = UserModel.createUser(userObject);
+         // generate tokens 
+         const token = jwt.sign({userId:newUser.id,telephone},"RANDOM_TOKEN_SECRET",{expiresIn:"24h"});
+         // final response;
+         res.status(201).json({status:201,data:{token,id:newUser.id,firstName,surName,telephone,email,nationality,category}});
+     }
     
-
-            }
-        })
-    
-         
-    
-    
-    }
  }
 
 }
 
 
-    static login(req,res){
+static login(req,res)   {
+    // fetch telephone from the the body request
   const { telephone } = req.body;
+  //check on validation 
   const validationObject = { telephone };
   const { error } = Validation.loginValidation(validationObject);
      if(error){
       res.status(400).json({status:400,error:`bad Input ${error}`});
-  }else{
-     console.log(telephone);       
+  }else{    
+      // find user from the 
       const user = UserModel.findUser(telephone);
       if(user){
-        console.log(user + 'hello');
-          // compare passwords 
-          bcrypt.compare(telephone,user.hashedTelephone,(err,isMatched)=>{
-         if(err){
-            console.log('hello2')
-             res.status(500).json({status:500,error:`Internal server error:${err}`})
-         }else if(isMatched){
-            console.log('hello3')
-            const token = jwt.sign({userId:newUser.id,telephone},"RANDOM_TOKEN_SECRET",{ expiresIn:"24"});
-            res.status(201).json({status:201,data:token});
-         }else{
-            console.log('hello4')
-             res.status(401).json({status:401,error:"phonenuber is incorrect one"})
-         }
-          });
+             // create tokens 
+          const token = jwt.sign({userId:user.id,telephone},"RANDOM_TOKEN_SECRET",{expiresIn:"24h"});
+          // final response 
+          res.status(201).json({status:201,data:token});
       }else{
-          res.status(401).json({status:401,error:`User is not found`});
+        res.status(404).json({status:404,error:"user is not found "});
       }
-  }
+    }
+}
 }
 
-}
+
 
 
 
