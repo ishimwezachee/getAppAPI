bc838f6790a82721d3a4450e1d160f875626738e
zachee <zacheeishimwe@gmail.com>
2019-10-30 14:33:25 +0200

    get other occupation informaton

--- src/controllers/occupation/other.js
+++ src/controllers/occupation/other.js
@@ -4,7 +4,7 @@ import OtherInformationModel from "../../models/occupation/other";
 // a class that holds all the logic
 
 class OtherInformationControllers{
-    static createOtherInfomation(req,res){
+    static createOtherInformation(req,res){
         const {
             businessType,
             businessName,
@@ -41,6 +41,30 @@ class OtherInformationControllers{
         })
     }
     }
+
+    // view a specific otherOccupation information
+    static viewSpecificOtherOccupationInformation(req,res){
+        const OtherInfoId = Number(req.params.OtherInfoId);
+        // check if the data exists;
+        const OneOtherOccupationInformation = OtherInformationModel.findOne(OtherInfoId);
+        if(OneOtherOccupationInformation){
+            res.status(200).json({
+                status:200,
+                data:OneOtherOccupationInformation
+            })
+        }
+    };
+
+    // view all otherOccupation information;
+    static ViewAllOtherOccupationInformation(req,res){
+        const OtherOccupationInformation = OtherInformationModel.findAllOtherOccupationInformation();
+        res.status(200).json({
+            status:200,
+            data:OtherOccupationInformation
+        })
+    }
+
 }
+
 
 export default OtherInformationControllers;
